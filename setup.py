2ae2b74bfe80bf3e7fead8139172a222a1ff4759
Qiao Liang <liang@Qiaos-MacBook-Pro.local>
2020-10-13 10:10:02 +0800

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bp-btc-com",
-    version="0.0.2",
+    version="0.0.3",
     author="Joe Pasquantonio/Qiao Liang",
     author_email="liang.qiao@binance.com",
     description="An api wrapper for btc.com block explorer api",
